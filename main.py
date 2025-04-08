35a752d7078d3fd3a5af0292647454a8cde7ec2c
Myczes123 <michalsolowiej07@gmail.com>
2025-04-08 18:31:25 +0200

    wip: data structure

---
+++ main.py
@@ -0,0 +1,15 @@
+
+
+users=[
+{"name":"Beata","Location":"Lublin","posts":500},
+{"name":"Michal","Location":"Krasnystaw","posts":420},
+{"name":"Ksavier","Location":"Grudziac","posts":690},
+{"name":"Damian","Location":"Kraków","posts":699},
+
+]
+
+
+
+Twoj znaomy z opublikowal tyle i tyle postow
+for user in users:
+    print(f"twoj zajomy {user['name']}!"),
