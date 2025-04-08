e9c5efc04ad983c838105cac3d72ab9ee87e6bb8
Myczes123 <michalsolowiej07@gmail.com>
2025-04-08 19:01:16 +0200

    chore:mvt pattern

--- main.py
+++ main.py
@@ -1,19 +1,8 @@
+from utils.controller import get_user_info
+from utils.model import
 
-
-users=[
-{"name":"Beata","Location":"Lublin","posts":500},
-{"name":"Michal","Location":"Krasnystaw","posts":420},
-{"name":"Ksavier","Location":"Grudziac","posts":690},
-{"name":"Damian","Location":"Kraków","posts":699},
-
-]
-
-
-
-Twoj znaomy z opublikowal tyle i tyle postow
-for user in users:
-    print(f"twoj zajomy {user['name']} , z:{user['location']} , opublikowal :{user['posts'])
-    
-def get_user_info(users_data: list)->None:
-
+def main():
     get_user_info(users)
+
+    if__name__ == "__main__":
+    main()
