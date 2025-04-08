2b1ec85c59058811cd99091d05e740d84c48709c
Myczes123 <michalsolowiej07@gmail.com>
2025-04-08 19:16:42 +0200

    wip: preparet menu

--- main.py
+++ main.py
@@ -2,6 +2,19 @@ from utils.controller import get_user_info
 from utils.model import
 
 def main():
+    print("===================MENU===========")
+    print('0-zamknij aplikacje')
+    print('1-wyswietl co u zajomych ')
+    print('2-dodaj nowego uzytkownika ')
+    print('3-usun uzytkownika ')
+    print('4-edytuj uzytkownika ')
+    print("===================MENU===========")
+    while True:
+        choice = input('wybierz opcje menu')
+        if choice == '0':
+            break
+            if choice == '1':
+
     get_user_info(users)
 
     if__name__ == "__main__":
